5f877dab10a1fbaa1341183c48a9cd480b7c69bb
kyle <thonjarkrasri@gmail.com>
2025-12-26 16:20:36 +0700

    0101011

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std ;
+
+int main(){
+    string grade[9]  = {"A", "B+", "B", "C+", "C", "D+", "D", "F" , "W"}  ;
+    string null_; 
+    srand(time(0)) ; 
+    int random = rand()%9;
+    cout << "Press Enter 3 times to reveal your future."  ;
+
+    for (int i = 0 ; i < 3 ; i++){
+        getline(cin , null_) ;
+    }
+
+    cout << "You will get " << grade[random] << " in this 261102." ;
+
+}
